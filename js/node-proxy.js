b75b4dd10c4aa686c5a17a8de9b42bcdf146c96c
Lukáš Vlček <lukas.vlcek@gmail.com>
2010-10-01 15:49:04 +0200

    added fitlers for PUT and DELETE

--- js/node-proxy.js
+++ js/node-proxy.js
@@ -19,23 +19,32 @@
  *   to expose only *safe* operations thus clients can not modify, delete and add indices nor they can shutdown and
  *   restart cluster nodes.
  *
- * Example: the following command start the proxy on port 80 and allows only GET requests with '_search' in URL
+ * Example #1: start the proxy on port 80 and allows only GET requests with '_search' in URL
+ * $node node-proxy.js port=80 allow='{"GET":["_search"]}'
  *
- * $node node-proxy.js proxyPort=80 allowed='{"GET":["_search"]}'
+ * Example #2: start the proxy on default port 8124 with no restrictions on any GET, POST and OPTIONS requests
+ * $node node-proxy.js allow='{"GET":[".*"], "POST":[".*"], "OPTIONS":[".*"]}'
  *
+ * It is possible to set allow rules for any of te following requests: GET, POST, PUT, DELETE and OPTIONS
+ *
+ * Note: OPTIONS requests can be used by some clients for pre-flight requests. If no OPTIONS requests allowed
+ * then some clients may not work properly. Web Browsers such as Google Chrome or Firefox are known to use them.
  */
 
 var http = require('http');
 
 var defaults = {
     seeds : ["localhost:9200"],
-    //allowed : { "GET" : [".*"], "POST" : [".*"], "OPTIONS" : [".*"]},
-    allowed : { "GET" : ["(_search|_status|_mapping)","/.+/.+/.+/_mlt","/.+/.+/.+]"], "POST" : ["_search","/.+/.+/.+/_mlt"], "OPTIONS" : [".*"]},
+    allow : {
+        "GET" : ["(_search|_status|_mapping)","/.+/.+/.+/_mlt","/.+/.+/.+]"],
+        "POST" : ["_search","/.+/.+/.+/_mlt"],
+        "OPTIONS" : [".*"] // allow any pre-flight request
+    },
     refresh : 10,
-    proxyPort : 8124
+    port : 8124
 };
 var filters = {
-    "GET" : [], "POST" : [], "OPTIONS" : []
+    "GET" : [], "POST" : [], "OPTIONS" : [], "PUT" : [], "DELETE" : []
 };
 
 // verify command line arguments (they override defaults) and pre-compile RegExps
@@ -45,7 +54,7 @@ var parseArgs = function() {
             var p = process.argv[i].split("=",2);
             if (defaults[p[0]]) {
                 if (p[0] === "refresh") {defaults.refresh=parseInt(p[1])}
-                else if (p[0] === "proxyPort") {defaults.proxyPort=parseInt(p[1])}
+                else if (p[0] === "port") {defaults.port=parseInt(p[1])}
                 else {
                     var o = JSON.parse(p[1]);
                     if (p[0] === "seeds") {
@@ -54,15 +63,21 @@ var parseArgs = function() {
                         } else {
                             throw new Error("parameter [seeds] is not a valid non-empty json array");
                         }
-                    } else if (p[0] === "allowed") {
+                    } else if (p[0] === "allow") {
                         if (typeof o ==="object" && o["join"] === undefined) {
-                           defaults.allowed = o;
+                           defaults.allow = o;
                         } else {
                             throw new Error("parameter [allowed] must be valid json object, not array");
     }}}}}}
-    if (defaults.allowed.GET) for (i=0;i<defaults.allowed.GET.length;i++) { filters.GET.push(new RegExp(defaults.allowed.GET[i])); }
-    if (defaults.allowed.POST) for (i=0;i<defaults.allowed.POST.length;i++) { filters.POST.push(new RegExp(defaults.allowed.POST[i])); }
-    if (defaults.allowed.OPTIONS) for (i=0;i<defaults.allowed.OPTIONS.length;i++) { filters.OPTIONS.push(new RegExp(defaults.allowed.OPTIONS[i])); }
+    for (type in filters) {
+        if (defaults.allow[type])
+            for (i=0;i<defaults.allow[type].length;i++) {
+                 filters[type].push(new RegExp(defaults.allow[type][i]));
+            }
+    }
+//    if (defaults.allow.GET) for (i=0;i<defaults.allow.GET.length;i++) { filters.GET.push(new RegExp(defaults.allow.GET[i])); }
+//    if (defaults.allow.POST) for (i=0;i<defaults.allow.POST.length;i++) { filters.POST.push(new RegExp(defaults.allow.POST[i])); }
+//    if (defaults.allow.OPTIONS) for (i=0;i<defaults.allow.OPTIONS.length;i++) { filters.OPTIONS.push(new RegExp(defaults.allow.OPTIONS[i])); }
 }
 
 try { parseArgs(); }
@@ -102,7 +117,7 @@ http.createServer(function (req, res) {
                         jsource = JSON.parse(jres);
 //                        delete jsource._shards; // TODO implement better filtering of response output
                     }
-                    { // why do I have to remove content-length and add chunked get it working?
+                    { // why do I have to remove content-length and add chunked to get it work correctly?
                         delete response.headers["Content-Length"];
                         response.headers["Transfer-Encoding"] = "chunked";
                     }
@@ -117,5 +132,5 @@ http.createServer(function (req, res) {
         res.writeHead(403, {'Content-Type': 'application/json'});
         res.end('{"error":"Request not supported by proxy"}');
     }
-}).listen(defaults.proxyPort, "127.0.0.1");
-console.log('ElasticSearch proxy server started at http://127.0.0.1:'+defaults.proxyPort+'/');
+}).listen(defaults.port, "127.0.0.1");
+console.log('ElasticSearch proxy server started at http://127.0.0.1:'+defaults.port+'/');
